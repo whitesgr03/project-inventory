f5a9f32b3929aa7bf03ffef955c01df5e6f2932e
Weiss Bai <whitesgr03@gmail.com>
2024-02-16 16:13:52 +0800

    Use helmet

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
 
@@ -7,6 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(compression());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
