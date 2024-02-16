7eaa21caf6078c430c62f5806c60eeddedcc95aa
Weiss Bai <whitesgr03@gmail.com>
2024-02-16 16:13:41 +0800

    Use compression

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const compression = require("compression");
 
 var app = express();
 
@@ -13,7 +6,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
