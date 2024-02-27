b2cdb7a63801b410a0a314ab1e15e4a7fc3a7ac1
Weiss Bai <whitesgr03@gmail.com>
2024-02-27 15:52:31 +0800

    Replace userRouter to inventoryRouter

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/inventory", inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
