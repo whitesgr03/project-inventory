99cce9f2470d601f10d11ecf2781e9124d913324
Weiss Bai <whitesgr03@gmail.com>
2024-02-29 16:05:55 +0800

    Change home page middleware
    Use redirection instead of rendering index template.

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const asyncHandler = require("express-async-handler");
 
-const Category = require("../models/category");
-const Products = require("../models/product");
-
-router.get(
-	"/",
-	asyncHandler(async (req, res, next) => {
-		const numCategories = Category.countDocuments().exec();
-		const numProducts = Products.countDocuments().exec();
-
-		res.render("index", {
-			title: "Project Y",
-			categories_count: await numCategories,
-			products_count: await numProducts,
-		});
-	})
-);
+router.get("/", (req, res) => {
+	res.redirect("/inventory");
+});
 
 module.exports = router;
