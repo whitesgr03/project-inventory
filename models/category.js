8a8ef3b8175ea472737b736ccedb1af1c19ac561
Weiss Bai <whitesgr03@gmail.com>
2024-03-06 11:48:49 +0800

    Change maxLength validator to 30

--- models/category.js
+++ models/category.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema(
 	{
-		name: { type: String, required: true, maxLength: 100 },
+		name: { type: String, required: true, maxLength: 30 },
 		description: { type: String, required: true },
 	},
 	{
