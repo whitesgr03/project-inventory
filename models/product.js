99d5983801deab7048bbb0fcb9e7c3d9cbe74378
Weiss Bai <whitesgr03@gmail.com>
2024-03-08 19:45:28 +0800

    Remove schema type option maxLength

--- models/product.js
+++ models/product.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema(
 	{
-		name: { type: String, required: true, maxLength: 100 },
+		name: { type: String, required: true },
 		description: { type: String, required: true },
 		category: {
 			type: Schema.Types.ObjectId,
