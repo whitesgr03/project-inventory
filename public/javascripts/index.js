c7ffa3a6d16e75380bc1e960a0de6f21fc7cdaf3
Weiss Bai <whitesgr03@gmail.com>
2024-03-15 16:34:50 +0800

    Merge functions
    Put "handleBurger" and "changeTheme" into "handleClick" function.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,44 +1,28 @@
 const handleClick = e => {
+	const hamburgerBtn = document.querySelector(".hamburger button > div");
+	const sidebar = document.querySelector("nav");
+
+	const handleCloseHamburger = () => {
+		hamburgerBtn.classList.remove("active");
+		sidebar.classList.remove("show");
+	};
+
+	const handleActiveHamburger = () => {
+		hamburgerBtn.classList.toggle("active");
+		sidebar.classList.toggle("show");
+	};
+
+	const handleChangeTheme = () =>
+		localStorage.setItem(
+			"darkScheme",
+			JSON.stringify(document.body.classList.toggle("dark"))
+		);
+
 	!e.target.closest(".hamburger") &&
 		!e.target.closest("nav") &&
 		handleCloseHamburger();
 	e.target.closest(".hamburger") && handleActiveHamburger();
 	e.target.closest(".theme") && handleChangeTheme();
 };
-
-const handleCloseHamburger = () => {
-	const hamburgerBtn = document.querySelector(".hamburger button > div");
-	const sidebar = document.querySelector("nav");
-	hamburgerBtn.classList.remove("active");
-	sidebar.classList.remove("show");
-};
-
-const handleActiveHamburger = () => {
-	const hamburgerBtn = document.querySelector(".hamburger button > div");
-	const sidebar = document.querySelector("nav");
-	hamburgerBtn.classList.toggle("active");
-	sidebar.classList.toggle("show");
-};
-
-const handleChangeTheme = () =>
-	localStorage.setItem(
-		"darkScheme",
-		JSON.stringify(document.body.classList.toggle("dark"))
-	);
-
-const setDarkScheme = () => {
-	const darkScheme =
-		window.matchMedia("(prefers-color-scheme: dark)")?.matches ?? false;
-
-	localStorage.setItem("darkScheme", JSON.stringify(darkScheme));
-
-	return darkScheme;
-};
-
-const getDarkScheme = () =>
-	(JSON.parse(localStorage.getItem("darkScheme")) ?? setDarkScheme()) &&
-	document.body.classList.add("dark");
-
-getDarkScheme();
 
 document.body.addEventListener("click", handleClick);
