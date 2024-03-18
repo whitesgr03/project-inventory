411dc3dde44ba46190bf49eff096e558f41e4391
Weiss Bai <whitesgr03@gmail.com>
2024-03-18 10:46:12 +0800

    Add error option to unknown routes handler

--- app.js
+++ app.js
@@ -54,9 +54,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/inventory", inventoryRouter);
 
-// catch 404 and forward to error handler
+// unknown routes handler
 app.use((req, res, next) => {
-	next(createError(404));
+	next(createError(404, "Page not found", { type: "page" }));
 });
 
 // error handler
