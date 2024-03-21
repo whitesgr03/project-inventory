e7d3c53a866f6a852263abc900204f1ae461b1b9
Weiss Bai <whitesgr03@gmail.com>
2024-03-21 21:46:06 +0800

    Change productUpdateGet function
    Use "try" and "catch" to handle errors.
    
    In "try" we use a condition to check if the "currentProduct"
    is found, if not then call "next" and pass the result of the
    "createError" function, if so then check whether NODE_ENV environment
    variable is "development" or "currentProduct" has a "expiresAfter"
    property, if so then we calls "validationFields" function, if not,
    we will redirect to the "currentProduct's" details page.
    
    The "validationFields" function validates all fields. If there
    is an error in the result, "renderErrorMessages" will be called
    to render the productForm template with the error messages.
    If not, "updateProduct" will be called to handle the update.
    
    In "catch" we call "next" and pass the result of the "createError"
    function.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const asyncHandler = require("express-async-handler");
 const createError = require("http-errors");
 const { Storage } = require("@google-cloud/storage");
@@ -235,10 +236,209 @@ const productUpdateGet = async (req, res, next) => {
 	}
 };
 const productUpdatePost = [
-	asyncHandler(async (req, res, next) => {
-		res.send("This is product update post page");
-	}),
+	uploadFile.single("image"),
+	async (req, res, next) => {
+		try {
+			const currentProduct = await Product.findById(req.params.id).exec();
+			const categories = await Category.find({}, { name: 1 })
+				.sort({ name: 1 })
+				.exec();
+			const validationFields = async () => {
+				const validationSchema = {
+					name: {
+						trim: true,
+						isLength: {
+							options: { min: 1, max: 100 },
+							errorMessage:
+								"The name must be less than 100 long.",
+						},
+						escape: true,
+						custom: {
+							options: value =>
+								new Promise(async (resolve, reject) => {
+									const existingProduct =
+										await Product.findOne({
+											$and: [
+												{ name: value },
+												{
+													_id: {
+														$ne: new ObjectId(
+															req.params.id
+														),
+													},
+												},
+											],
+										}).exec();
+
+									existingProduct ? reject() : resolve();
+								}),
+							errorMessage: "The name is been used.",
+						},
+					},
+					category: {
+						trim: true,
+						escape: true,
+						custom: {
+							options: value =>
+								typeof value === "string" &&
+								categories.find(
+									category =>
+										category._id.toString() === value
+								),
+							errorMessage: "The category must be chosen.",
+						},
+					},
+					price: {
+						trim: true,
+						isFloat: {
+							options: { min: 1 },
+							errorMessage: "The price must be input.",
+						},
+						escape: true,
+					},
+					quantity: {
+						trim: true,
+						isInt: {
+							options: { min: 1, max: 999 },
+							errorMessage: "The quantity must be input.",
+						},
+						escape: true,
+					},
+					description: {
+						trim: true,
+						notEmpty: {
+							errorMessage: "The description must be input.",
+						},
+						escape: true,
+					},
+				};
+
+				const nameFiled = req.body.name;
+
+				await checkSchema(validationSchema, ["body"]).run(req);
+
+				const schemaErrors = validationResult(req);
+
+				const uploadImage = req.file;
+				const imageInfo =
+					uploadImage &&
+					uploadImage.mimetype === "image/jpeg" &&
+					(await sharp(uploadImage.buffer).metadata());
+
+				const validateUploadImage = () => {
+					const errorMessage = {
+						msg: "The image is required, size must be less than 500 kb, width and height must be 800 or greater.",
+					};
+
+					return (
+						(!imageInfo ||
+							imageInfo.size > 500000 ||
+							imageInfo.width < 800 ||
+							imageInfo.height < 800) &&
+						errorMessage
+					);
+				};
+
+				const uploadImageError = validateUploadImage();
+
+				const product = {
+					_id: req.params.id,
+					...req.body,
+				};
+
+				const updateProduct = async () => {
+					const currentTime = new Date();
+					const deleteProductImage = async () => {
+						const productImageName = `${unescape(
+							currentProduct.name
+						).replace(
+							/[^a-z0-9]+/gi,
+							"-"
+						)}-${+currentProduct.lastModified}.jpg`;
+
+						await googleStorage
+							.bucket(bucketName)
+							.file(productImageName)
+							.delete();
+					};
+
+					const uploadNewProductImage = async () => {
+						const newProductImageName = `${nameFiled.replace(
+							/[^a-z0-9]+/gi,
+							"-"
+						)}-${+currentTime}.jpg`;
+						const resizeImageBuffer = async () =>
+							await sharp(uploadImage.buffer)
+								.resize({ width: 800, height: 800 })
+								.jpeg({ mozjpeg: true })
+								.toBuffer();
+
+						const imageBuffer =
+							imageInfo.width > 800 || imageInfo.height > 800
+								? await resizeImageBuffer()
+								: uploadImage.buffer;
+
+						await googleStorage
+							.bucket(bucketName)
+							.file(newProductImageName)
+							.save(imageBuffer);
+					};
+
+					const editProduct = async () => {
+						const newProduct = new Product({
+							...product,
+							lastModified: currentTime,
+						});
+						await Product.findByIdAndUpdate(
+							product._id,
+							newProduct
+						).exec();
+						res.redirect(newProduct.url);
+					};
+
+					await deleteProductImage();
+					await uploadNewProductImage();
+					await editProduct();
+				};
+
+				const renderErrorMessages = () => {
+					const errors = schemaErrors.mapped();
+					uploadImageError && (errors.image = uploadImageError);
+
+					res.render("productForm", {
+						title: "Update product",
+						product,
+						categories,
+						errors,
+					});
+				};
+
+				schemaErrors.isEmpty()
+					? updateProduct()
+					: renderErrorMessages();
+			};
+			currentProduct
+				? process.env.NODE_ENV === "development" ||
+				  currentProduct.expiresAfter
+					? validationFields()
+					: res.redirect(currentProduct.url)
+				: next(
+						createError(404, "Product not found", {
+							type: "product",
+						})
+				  );
+		} catch (err) {
+			console.log("err", err);
+			next(
+				createError(400, "Product not found", {
+					cause: process.env.NODE_ENV === "development" ? err : {},
+					type: "product",
+				})
+			);
+		}
+	},
 ];
+
 const productDeleteGet = asyncHandler(async (req, res, next) => {
 	res.send("This is product delete get page");
 });
