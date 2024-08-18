9bb0a7d3877572a163cf7246ba14959f4d0fc505
Weiss Bai <whitesgr03@gmail.com>
2024-08-18 08:41:49 +0800

    Create database configuration

---
+++ config/database.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import debug from "debug";
+
+const databaseLog = debug("Mongoose");
+
+const handleError = err => {
+	databaseLog("Database connecting error");
+	databaseLog(err);
+	mongoose.disconnect();
+	databaseLog(`Database is disconnected.`);
+	process.exit(1);
+};
+
+const db = mongoose.connection;
+
+db.on("connecting", () => databaseLog("Connecting MongoDB..."));
+
+mongoose.connect(process.env.DATABASE_URL).catch(err => handleError(err));
+
+export default db;
