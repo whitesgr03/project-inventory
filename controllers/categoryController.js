3bab33e9fbea8b320cda589fce2c62834a91c1e9
Weiss Bai <whitesgr03@gmail.com>
2024-08-20 21:35:45 +0800

    Refactor category controller
    Add asyncHandler instead of try catch to all middleware.
    
    Delete escape sanitizer for each validation schema.
    
    The "expiresAfter" field for create and update categories will now
    be set to one day instead of ten minutes.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,7 @@
 import { Types } from "mongoose";
 import createError from "http-errors";
 import asyncHandler from "express-async-handler";
-import { validationResult, checkSchema } from "express-validator";
+import { validationResult, checkSchema, matchedData } from "express-validator";
 
 import Category from "../models/category.js";
 import Product from "../models/product.js";
@@ -25,46 +25,34 @@ const categoryList = asyncHandler(async (req, res, next) => {
 		categories,
 	});
 });
-const categoryDetail = async (req, res, next) => {
-	try {
-		const [category, products] = await Promise.all([
-			Category.findById(req.params.id).exec(),
-			Product.find({ category: req.params.id }, { name: 1 })
-				.sort({ name: 1 })
-				.exec(),
-		]);
+const categoryDetail = asyncHandler(async (req, res, next) => {
+	const [category, products] = await Promise.all([
+		Category.findById(req.params.id).exec(),
+		Product.find({ category: req.params.id }, { name: 1 })
+			.sort({ name: 1 })
+			.exec(),
+	]);
 
-		category
-			? res.render("categoryDetail", {
-					category,
-					products,
-			  })
-			: next(
-					createError(404, "Category not found", { type: "category" })
-			  );
-	} catch (err) {
-		next(
-			createError(400, "Category not found", {
-				cause: process.env.NODE_ENV === "development" ? err : {},
-				type: "category",
-			})
-		);
-	}
-};
-const categoryCreateGet = asyncHandler(async (req, res, next) => {
+	category
+		? res.render("categoryDetail", {
+				category,
+				products,
+		  })
+		: next(createError(404, "Category not found", { type: "category" }));
+});
+const categoryCreateGet = async (req, res) =>
 	res.render("categoryForm", {
 		title: "Add a new category",
 	});
-});
-const categoryCreatePost = asyncHandler(async (req, res, next) => {
+const categoryCreatePost = asyncHandler(async (req, res) => {
 	const validationSchema = {
 		name: {
 			trim: true,
 			isLength: {
-				options: { min: 1, max: 30 },
-				errorMessage: "The name length must be 1 to 30.",
+				options: { max: 30 },
+				errorMessage: "The name must be less than 30 long.",
+				bail: true,
 			},
-			escape: true,
 			custom: {
 				options: value =>
 					new Promise(async (resolve, reject) => {
@@ -79,221 +67,161 @@ const categoryCreatePost = asyncHandler(async (req, res, next) => {
 		description: {
 			trim: true,
 			notEmpty: { errorMessage: "The description must be input." },
-			escape: true,
 		},
 	};
-
 	await checkSchema(validationSchema, ["body"]).run(req);
-
 	const schemaErrors = validationResult(req);
-
-	const category = {
-		...req.body,
-	};
-
 	const addNewCategory = async () => {
+		const category = matchedData(req);
 		const newCategory = new Category(category);
+		const oneDay = 24 * 60 * 60 * 1000;
 
-		const tenMinutes = 10 * 60 * 1000;
-		process.env.NODE_ENV === "production" &&
-			(newCategory.expiresAfter = new Date(Date.now() + tenMinutes));
+		newCategory.expiresAfter = new Date(Date.now() + oneDay);
 
 		await newCategory.save();
 		res.redirect(newCategory.url);
 	};
-
 	const renderErrorMessages = () => {
 		res.render("categoryForm", {
 			title: "Add a new category",
-			category,
+			category: {
+				...req.body,
+			},
 			errors: schemaErrors.mapped(),
 		});
 	};
-
 	schemaErrors.isEmpty() ? addNewCategory() : renderErrorMessages();
 });
-const categoryUpdateGet = async (req, res, next) => {
-	try {
-		const category = await Category.findById(req.params.id).exec();
+const categoryUpdateGet = asyncHandler(async (req, res, next) => {
+	const category = await Category.findById(req.params.id).exec();
 
-		category
-			? process.env.NODE_ENV === "development" || category.expiresAfter
-				? res.render("categoryForm", {
-						title: "Update category",
-						category,
-				  })
-				: res.redirect(category.url)
-			: next(
-					createError(404, "Category not found", { type: "category" })
-			  );
-	} catch (err) {
-		next(
-			createError(400, "Category not found", {
-				cause: process.env.NODE_ENV === "development" ? err : {},
-				type: "category",
-			})
-		);
-	}
-};
-const categoryUpdatePost = async (req, res, next) => {
-	try {
-		const category = await Category.findById(req.params.id).exec();
-		const validationFields = async () => {
-			const validationSchema = {
-				name: {
-					trim: true,
-					isLength: {
-						options: { min: 1, max: 30 },
-						errorMessage: "The name length must be 1 to 30.",
-					},
-					escape: true,
-					custom: {
-						options: value =>
-							new Promise(async (resolve, reject) => {
-								const existingCategory = await Category.findOne(
-									{
-										$and: [
-											{ name: value },
-											{
-												_id: {
-													$ne: Types.ObjectId.createFromHexString(
-														req.params.id
-													),
-												},
-											},
-										],
-									}
-								).exec();
-								existingCategory ? reject() : resolve();
-							}),
-						errorMessage: "The name is been used.",
-					},
-				},
-				description: {
-					trim: true,
-					notEmpty: {
-						errorMessage: "The description must be input.",
-					},
-					escape: true,
-				},
-			};
-
-			await checkSchema(validationSchema, ["body"]).run(req);
-
-			const schemaErrors = validationResult(req);
-
-			const newCategory = new Category({
-				_id: req.params.id,
-				...req.body,
-			});
-
-			const updateCategory = async () => {
-				await Category.findByIdAndUpdate(
-					newCategory._id,
-					newCategory
-				).exec();
-				res.redirect(newCategory.url);
-			};
-
-			const renderErrorMessages = () => {
-				res.render("categoryForm", {
+	category
+		? !category?.expiresAfter
+			? res.redirect(category.url)
+			: res.render("categoryForm", {
 					title: "Update category",
-					category: newCategory,
-					errors: schemaErrors.mapped(),
-				});
-			};
-
-			schemaErrors.isEmpty() ? updateCategory() : renderErrorMessages();
+					category,
+			  })
+		: next(createError(404, "Category not found", { type: "category" }));
+});
+const categoryUpdatePost = asyncHandler(async (req, res, next) => {
+	const category = await Category.findById(req.params.id).exec();
+	const validationFields = async () => {
+		const validationSchema = {
+			name: {
+				trim: true,
+				isLength: {
+					options: { max: 30 },
+					errorMessage: "The name must be less than 30 long.",
+					bail: true,
+				},
+				custom: {
+					options: value =>
+						new Promise(async (resolve, reject) => {
+							const existingCategory = await Category.findOne({
+								$and: [
+									{ name: value },
+									{
+										_id: {
+											$ne: Types.ObjectId.createFromHexString(
+												req.params.id
+											),
+										},
+									},
+								],
+							}).exec();
+							existingCategory ? reject() : resolve();
+						}),
+					errorMessage: "The name is been used.",
+				},
+			},
+			description: {
+				trim: true,
+				notEmpty: {
+					errorMessage: "The description must be input.",
+				},
+			},
 		};
 
-		category
-			? process.env.NODE_ENV === "development" || category.expiresAfter
-				? validationFields()
-				: res.redirect(category.url)
-			: next(
-					createError(404, "Category not found", {
-						type: "category",
-					})
-			  );
-	} catch (err) {
-		next(
-			createError(400, "Category not found", {
-				cause: process.env.NODE_ENV === "development" ? err : {},
-				type: "category",
-			})
-		);
-	}
-};
-const categoryDeleteGet = async (req, res, next) => {
-	try {
-		const [category, products] = await Promise.all([
-			Category.findById(req.params.id).exec(),
-			Product.find({ category: req.params.id }, { name: 1 })
-				.sort({ name: 1 })
-				.exec(),
-		]);
+		await checkSchema(validationSchema, ["body"]).run(req);
+		const schemaErrors = validationResult(req);
 
-		const renderTemplate = () => {
-			const locals = {
+		const updateCategory = async () => {
+			await Category.findByIdAndUpdate(req.params.id, {
+				...matchedData(req),
+			}).exec();
+			res.redirect(category.url);
+		};
+		const renderErrorMessages = () => {
+			res.render("categoryForm", {
+				title: "Update category",
 				category,
-				products,
-			};
-
-			products.length && (locals.remove = true);
-			!products.length && (locals.title = "Category delete");
-
-			res.render("categoryDetail", locals);
+				errors: schemaErrors.mapped(),
+			});
 		};
 
-		category
-			? process.env.NODE_ENV === "development" || category.expiresAfter
-				? renderTemplate()
-				: res.redirect(category.url)
-			: next(
-					createError(404, "Category not found", {
-						type: "category",
-					})
-			  );
-	} catch (err) {
-		next(
-			createError(400, "Category not found", {
-				cause: process.env.NODE_ENV === "development" ? err : {},
-				type: "category",
-			})
-		);
-	}
-};
-const categoryDeletePost = async (req, res, next) => {
-	try {
-		const [category, products] = await Promise.all([
-			Category.findById(req.params.id).exec(),
-			Product.find({ category: req.params.id }).exec(),
-		]);
+		schemaErrors.isEmpty() ? updateCategory() : renderErrorMessages();
+	};
+	category
+		? !category?.expiresAfter
+			? res.redirect(category.url)
+			: validationFields()
+		: next(
+				createError(404, "Category not found", {
+					type: "category",
+				})
+		  );
+});
+const categoryDeleteGet = asyncHandler(async (req, res, next) => {
+	const [category, products] = await Promise.all([
+		Category.findById(req.params.id).exec(),
+		Product.find({ category: req.params.id }, { name: 1 })
+			.sort({ name: 1 })
+			.exec(),
+	]);
 
-		const deleteCategory = async () => {
-			await Category.findByIdAndDelete(req.params.id).exec();
-			res.redirect("/inventory/categories");
-		};
+	const renderTemplate = () => {
+		products.length
+			? (res.locals.alert = true)
+			: (res.locals.title = "Category delete");
 
-		category
-			? process.env.NODE_ENV === "development" ||
-			  (category.expiresAfter && !products.length)
-				? deleteCategory()
-				: res.redirect(category.url)
-			: next(
-					createError(404, "Category not found", {
-						type: "category",
-					})
-			  );
-	} catch (err) {
-		next(
-			createError(400, "Category not found", {
-				cause: process.env.NODE_ENV === "development" ? err : {},
-				type: "category",
-			})
-		);
-	}
-};
+		res.render("categoryDetail", {
+			category,
+			products,
+		});
+	};
+
+	category
+		? !category?.expiresAfter
+			? res.redirect(category.url)
+			: renderTemplate()
+		: next(
+				createError(404, "Category not found", {
+					type: "category",
+				})
+		  );
+});
+const categoryDeletePost = asyncHandler(async (req, res, next) => {
+	const [category, products] = await Promise.all([
+		Category.findById(req.params.id).exec(),
+		Product.find({ category: req.params.id }).exec(),
+	]);
+
+	const deleteCategory = async () => {
+		await Category.findByIdAndDelete(req.params.id).exec();
+		res.redirect("/inventory/categories");
+	};
+
+	category
+		? !category?.expiresAfter
+			? res.redirect(category.url)
+			: !products.length && deleteCategory()
+		: next(
+				createError(404, "Category not found", {
+					type: "category",
+				})
+		  );
+});
 export {
 	index,
 	categoryList,
