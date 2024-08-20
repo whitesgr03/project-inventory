7b7cd3331e6223accf83fe07579ea08615d809ab
Weiss Bai <whitesgr03@gmail.com>
2024-08-20 21:02:43 +0800

    Change path table name to plural

--- routes/inventory.js
+++ routes/inventory.js
@@ -8,34 +8,34 @@ import * as productControllers from "../controllers/productController.js";
 router.get("/", categoryControllers.index);
 
 router
-	.route("/category/create")
+	.route("/categories/create")
 	.get(categoryControllers.categoryCreateGet)
 	.post(categoryControllers.categoryCreatePost);
 router
-	.route("/category/:id/update")
+	.route("/categories/:id/update")
 	.get(categoryControllers.categoryUpdateGet)
 	.post(categoryControllers.categoryUpdatePost);
 router
-	.route("/category/:id/delete")
+	.route("/categories/:id/delete")
 	.get(categoryControllers.categoryDeleteGet)
 	.post(categoryControllers.categoryDeletePost);
-	
-router.get("/category/:id", categoryControllers.categoryDetail);
+
+router.get("/categories/:id", categoryControllers.categoryDetail);
 router.get("/categories", categoryControllers.categoryList);
 
 router
-	.route("/product/create")
+	.route("/products/create")
 	.get(productControllers.productCreateGet)
 	.post(productControllers.productCreatePost);
 router
-	.route("/product/:id/update")
+	.route("/products/:id/update")
 	.get(productControllers.productUpdateGet)
 	.post(productControllers.productUpdatePost);
 router
-	.route("/product/:id/delete")
+	.route("/products/:id/delete")
 	.get(productControllers.productDeleteGet)
 	.post(productControllers.productDeletePost);
-router.get("/product/:id", productControllers.productDetail);
+router.get("/products/:id", productControllers.productDetail);
 router.get("/products", productControllers.productList);
 
 export default router;
